d67e7dcc9828ad4b3959b277df332f67d25e7323
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-10-22 19:45:12 +0200

    Fix script

--- Resources/Scripts/ido.sh
+++ Resources/Scripts/ido.sh
@@ -2,8 +2,8 @@
 
 # Build an Android kernel that is actually UEFI disguised as the Kernel
 cat ./BootShim/AARCH64/BootShim.bin "./Build/idoPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/IDO_UEFI.fd" > "./Build/idoPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/IDO_UEFI.fd-bootshim"||exit 1
-gzip -c < "./Build/idoPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/IDO_UEFI.fd-bootshim" > "./Build/idoPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/IDO_UEFI.fd-bootshim.gz"||exit 1
-cat "./Build/idoPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/IDO_UEFI.fd-bootshim.gz" ./Resources/DTBs/ido.dtb > ./Resources/bootpayload.bin||exit 1
+gzip -c < "./Build/idoPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/IDO_UEFI.fd-bootshim" >./uefi.img
+cat ./Resources/DTBs/ido.dtb > ./uefi.img||exit 1
 
 # Create bootable Android boot.img
 python3 ./Resources/Scripts/mkbootimg.py \
