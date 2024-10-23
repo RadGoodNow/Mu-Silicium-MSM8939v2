5f1fdcf8054fd2a4ea2fd276d8afd2be6262f2ea
rad <81907382+RadGoodNow@users.noreply.github.com>
2024-10-23 11:07:29 +0400

    TEST: ido.sh: fix appending dtb to image

--- Resources/Scripts/ido.sh
+++ Resources/Scripts/ido.sh
@@ -2,7 +2,7 @@
 
 # Build an Android kernel that is actually UEFI disguised as the Kernel
 cat ./BootShim/AARCH64/BootShim.bin "./Build/idoPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/IDO_UEFI.fd" > "./Build/idoPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/IDO_UEFI.fd-bootshim"||exit 1
-gzip -c < "./Build/idoPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/IDO_UEFI.fd-bootshim" >./uefi.img
-cat ./Resources/DTBs/ido.dtb > ./uefi.img||exit 1
+gzip -c < "./Build/idoPkg/${_TARGET_BUILD_MODE}_CLANGPDB/FV/IDO_UEFI.fd-bootshim" >./uefi.bin
+cat ./uefi.bin ./Resources/DTBs/ido.dtb > ./Mu-ido.img||exit 1
 
 
